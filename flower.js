e5c7ce748650ec73078f0840f194d3e9fa8cafea
Dilkhush-Salimi <mdaquilkulhaiya@gmail.com>
2025-08-16 15:37:52 +0530

    all set

---
+++ flower.js
@@ -0,0 +1,26 @@
+ const rounder=document.querySelector('.container')
+        const addflower=document.querySelector('button')
+            addflower.addEventListener('click', function(e){
+            if (e.target.textContent==='click to onn') {
+                let spa1=document.createElement('span')
+            let spa2=document.createElement('span')
+            let spa3=document.createElement('span')
+            let spa4=document.createElement('span')
+            let spa5=document.createElement('span')
+            let spa6=document.createElement('span')
+            let spa7=document.createElement('span')
+            rounder.appendChild(spa1);
+            rounder.appendChild(spa2);
+            rounder.appendChild(spa3);
+            rounder.appendChild(spa4);
+            rounder.appendChild(spa5);
+            rounder.appendChild(spa6);
+            rounder.appendChild(spa7);
+                
+            }else{
+                
+                
+               }
+            
+
+        })
